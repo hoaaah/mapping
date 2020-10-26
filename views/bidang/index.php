28404921d7e979bf463873e724fdf411c29e1134
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2020-10-26 10:43:56 +0700

    Opsi search sebelum post

--- views/bidang/index.php
+++ views/bidang/index.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\Modal;
 use kartik\grid\GridView;
-use johnitvn\ajaxcrud\CrudAsset; 
+use johnitvn\ajaxcrud\CrudAsset;
 use johnitvn\ajaxcrud\BulkButtonWidget;
 use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
@@ -23,53 +23,56 @@ CrudAsset::register($this);
 
 ?>
 <div class="ref-bidang-index">
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-    <?php $form = ActiveForm::begin(['id' => 'selection-gridview']); // 'action' => ['bulk-update'] ?>
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php $form = ActiveForm::begin(['id' => 'selection-gridview']); // 'action' => ['bulk-update'] 
+    ?>
     <div id="ajaxCrudDatatable">
-        <?=GridView::widget([
-            'id'=>'crud-datatable',
+        <?= GridView::widget([
+            'id' => 'crud-datatable',
             'dataProvider' => $dataProvider,
             // 'filterModel' => $searchModel,
-            'pjax'=>true,
-            'columns' => require(__DIR__.'/_columns.php'),
-            'toolbar'=> [
-                ['content'=>
-                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', [''],
-                    ['data-pjax'=>1, 'class'=>'btn btn-default', 'title'=>'Reset Grid']).
-                    '{toggleData}'.
-                    '{export}'
-                ],
-            ],          
+            'pjax' => true,
+            'columns' => require(__DIR__ . '/_columns.php'),
+            'toolbar' => [
+                ['content' =>
+                Html::a(
+                    '<i class="glyphicon glyphicon-repeat"></i>',
+                    [''],
+                    ['data-pjax' => 1, 'class' => 'btn btn-default', 'title' => 'Reset Grid']
+                ) .
+                    '{toggleData}' .
+                    '{export}'],
+            ],
             'striped' => true,
             'condensed' => true,
-            'responsive' => true,          
+            'responsive' => true,
             'panel' => [
-                'type' => 'primary', 
+                'type' => 'primary',
                 'heading' => '<i class="glyphicon glyphicon-list"></i> Ref Bidangs listing',
-                'before'=>'<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
-                'after'=> 
-                
-                '<div class="row"><div class="col-md-3">'.
-                $form->field($searchModel, 'kd_ubah')->widget(Select2::classname(), [
-                    'data' => ArrayHelper::map(JenisUbah::find()->all(), 'id', 'jenis'),
-                    'options' => ['placeholder' => 'Pilih Jenis Ubah ...'],
-                    'pluginOptions' => [
-                        'allowClear' => true
-                    ],
-                    'pjaxContainerId' => 'crud-datatable-pjax',
-                ])->label(false).
-                '</div><div class="col-md-3">'.
-                $form->field($searchModel, 'id_lama')->widget(Select2::classname(), [
-                    'data' => ArrayHelper::map(RefBidangLama::find()->select(['id', "CONCAT(kd_urusan, '.', kd_bidang, ' ', nm_bidang) AS nm_bidang"])->all(), 'id', 'nm_bidang'),
-                    'options' => ['placeholder' => 'Pilih Bidang Lama ...'],
-                    'pluginOptions' => [
-                        'allowClear' => true
-                    ],
-                    'pjaxContainerId' => 'crud-datatable-pjax',
-                ])->label(false).
-                '</div><div class="col-md-3">'.
-                Html::submitButton( '<i class="glyphicon glyphicon-arrow-right"></i> Submit Perubahan', ['id' => 'batch-submit', 'class' => 'btn btn-warning', 'data-confirm' => "Akan menyimpan data dana transfer pada belanja tersebut, pastikan data sudah benar.",]).
-                '</div></div>',
+                'before' => '<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
+                'after' =>
+
+                '<div class="row"><div class="col-md-3">' .
+                    $form->field($searchModel, 'kd_ubah')->widget(Select2::classname(), [
+                        'data' => ArrayHelper::map(JenisUbah::find()->all(), 'id', 'jenis'),
+                        'options' => ['placeholder' => 'Pilih Jenis Ubah ...'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                        'pjaxContainerId' => 'crud-datatable-pjax',
+                    ])->label(false) .
+                    '</div><div class="col-md-3">' .
+                    $form->field($searchModel, 'id_lama')->widget(Select2::classname(), [
+                        'data' => ArrayHelper::map(RefBidangLama::find()->select(['id', "CONCAT(kd_urusan, '.', kd_bidang, ' ', nm_bidang) AS nm_bidang"])->all(), 'id', 'nm_bidang'),
+                        'options' => ['placeholder' => 'Pilih Bidang Lama ...'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                        'pjaxContainerId' => 'crud-datatable-pjax',
+                    ])->label(false) .
+                    '</div><div class="col-md-3">' .
+                    Html::submitButton('<i class="glyphicon glyphicon-arrow-right"></i> Submit Perubahan', ['id' => 'batch-submit', 'class' => 'btn btn-warning', 'data-confirm' => "Akan menyimpan data dana transfer pada belanja tersebut, pastikan data sudah benar.",]) .
+                    '</div></div>',
                 // .
                 // BulkButtonWidget::widget([
                 //             'buttons'=>Html::a('<i class="glyphicon glyphicon-trash"></i>&nbsp; Delete All',
@@ -85,12 +88,12 @@ CrudAsset::register($this);
                 //         ]).                        
                 //         '<div class="clearfix"></div>',
             ]
-        ])?>
+        ]) ?>
         <?php ActiveForm::end(); ?>
     </div>
 </div>
 <?php Modal::begin([
-    "id"=>"ajaxCrudModal",
-    "footer"=>"",// always need it for jquery plugin
-])?>
+    "id" => "ajaxCrudModal",
+    "footer" => "", // always need it for jquery plugin
+]) ?>
 <?php Modal::end(); ?>
--- views/program/index.php
+++ views/program/index.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\Modal;
 use kartik\grid\GridView;
-use johnitvn\ajaxcrud\CrudAsset; 
+use johnitvn\ajaxcrud\CrudAsset;
 use johnitvn\ajaxcrud\BulkButtonWidget;
 use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
@@ -23,44 +23,47 @@ CrudAsset::register($this);
 
 ?>
 <div class="ref-program-index">
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-    <?php $form = ActiveForm::begin(['id' => 'selection-gridview']); // 'action' => ['bulk-update'] ?>
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php $form = ActiveForm::begin(['id' => 'selection-gridview']); // 'action' => ['bulk-update'] 
+    ?>
     <div id="ajaxCrudDatatable">
-        <?=GridView::widget([
-            'id'=>'crud-datatable',
+        <?= GridView::widget([
+            'id' => 'crud-datatable',
             'dataProvider' => $dataProvider,
             // 'filterModel' => $searchModel, 
             'pager' => [
                 'firstPageLabel' => 'Awal',
                 'lastPageLabel'  => 'Akhir'
             ],
-            'pjax'=>true,
-            'columns' => require(__DIR__.'/_columns.php'),
-            'rowOptions' => function ($model, $key, $index, $grid){
+            'pjax' => true,
+            'columns' => require(__DIR__ . '/_columns.php'),
+            'rowOptions' => function ($model, $key, $index, $grid) {
                 $klasifikasiLama = RefProgramLama::find()->where("ket_program LIKE '%{$model->ket_program}%'")->one();
-                if($klasifikasiLama){
-                    if($klasifikasiLama->kd_urusan == $model->kd_urusan && $klasifikasiLama->kd_bidang == $model->kd_bidang && $klasifikasiLama->kd_prog == $model->kd_prog) return ['class' => GridView::TYPE_DEFAULT];
+                if ($klasifikasiLama) {
+                    if ($klasifikasiLama->kd_urusan == $model->kd_urusan && $klasifikasiLama->kd_bidang == $model->kd_bidang && $klasifikasiLama->kd_prog == $model->kd_prog) return ['class' => GridView::TYPE_DEFAULT];
                 }
                 return ['class' => GridView::TYPE_WARNING]; // GridView::TYPE_DANGER 'kv-page-summary warning'
             },
-            'toolbar'=> [
-                ['content'=>
-                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', [''],
-                    ['data-pjax'=>1, 'class'=>'btn btn-default', 'title'=>'Reset Grid']).
-                    '{toggleData}'.
-                    '{export}'
-                ],
-            ],          
+            'toolbar' => [
+                ['content' =>
+                Html::a(
+                    '<i class="glyphicon glyphicon-repeat"></i>',
+                    [''],
+                    ['data-pjax' => 1, 'class' => 'btn btn-default', 'title' => 'Reset Grid']
+                ) .
+                    '{toggleData}' .
+                    '{export}'],
+            ],
             'striped' => true,
             'condensed' => true,
-            'responsive' => true,          
+            'responsive' => true,
             'panel' => [
-                'type' => 'primary', 
+                'type' => 'primary',
                 'heading' => '<i class="glyphicon glyphicon-list"></i> Ref Programs listing',
-                'before'=>'<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
-                'after'=>
-                
-                    '<div class="row"><div class="col-md-3">'.
+                'before' => '<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
+                'after' =>
+
+                '<div class="row"><div class="col-md-3">' .
                     $form->field($searchModel, 'kd_ubah')->widget(Select2::classname(), [
                         'data' => ArrayHelper::map(JenisUbah::find()->all(), 'id', 'jenis'),
                         'options' => ['placeholder' => 'Pilih Jenis Ubah ...'],
@@ -68,8 +71,8 @@ CrudAsset::register($this);
                             'allowClear' => true
                         ],
                         'pjaxContainerId' => 'crud-datatable-pjax',
-                    ])->label(false).
-                    '</div><div class="col-md-6">'.
+                    ])->label(false) .
+                    '</div><div class="col-md-6">' .
                     $form->field($searchModel, 'id_lama')->widget(Select2::classname(), [
                         'data' => ArrayHelper::map(RefProgramLama::find()->select(['id', "CONCAT(kd_urusan, '.', kd_bidang, '.', kd_prog, ' ', ket_program) AS ket_program"])->all(), 'id', 'ket_program'),
                         'options' => ['placeholder' => 'Pilih Program Lama ...'],
@@ -77,18 +80,18 @@ CrudAsset::register($this);
                             'allowClear' => true
                         ],
                         'pjaxContainerId' => 'crud-datatable-pjax',
-                    ])->label(false).
-                    '</div><div class="col-md-3">'.
-                    Html::submitButton( '<i class="glyphicon glyphicon-arrow-right"></i> Submit Perubahan', ['id' => 'batch-submit', 'class' => 'btn btn-warning', 'data-confirm' => "Akan menyimpan data dana transfer pada belanja tersebut, pastikan data sudah benar.",]).
+                    ])->label(false) .
+                    '</div><div class="col-md-3">' .
+                    Html::submitButton('<i class="glyphicon glyphicon-arrow-right"></i> Submit Perubahan', ['id' => 'batch-submit', 'class' => 'btn btn-warning', 'data-confirm' => "Akan menyimpan data dana transfer pada belanja tersebut, pastikan data sudah benar.",]) .
                     '</div></div>',
             ]
-        ])?>
+        ]) ?>
         <?php ActiveForm::end(); ?>
     </div>
 </div>
 <?php Modal::begin([
-    "id"=>"ajaxCrudModal",
+    "id" => "ajaxCrudModal",
     'size' => 'modal-lg',
-    "footer"=>"",// always need it for jquery plugin
-])?>
+    "footer" => "", // always need it for jquery plugin
+]) ?>
 <?php Modal::end(); ?>
